08eb2b8555dd7b29cdea89285bcf44a053ff5ee0
Andrew Stalick <astalick@rmn.com>
2017-02-27 14:51:39 -0600

    more standardization

--- environment.sh
+++ environment.sh
@@ -1,1 +1,18 @@
 # misc environment settings the don't warrant their own files
+
+# Set my editor and git editor
+export EDITOR="/usr/local/bin/atom -w"
+export GIT_EDITOR='/usr/local/bin/atom -w'
+
+#for a colorized man
+man() {
+    env \
+        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+        LESS_TERMCAP_md=$(printf "\e[1;31m") \
+        LESS_TERMCAP_me=$(printf "\e[0m") \
+        LESS_TERMCAP_se=$(printf "\e[0m") \
+        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+        LESS_TERMCAP_ue=$(printf "\e[0m") \
+        LESS_TERMCAP_us=$(printf "\e[1;32m") \
+            man "$@"
+}
--- node.sh
+++ node.sh
@@ -1,6 +1,7 @@
 # set up Node Version Manager
 export NVM_DIR="$HOME/.nvm"
 . "/usr/local/opt/nvm/nvm.sh"
+
 echo "NodeJS version"
 # echo out the version we're using
 nvm current
